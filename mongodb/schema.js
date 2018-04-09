5b1737dd2097d7452a5ac7378f6080957c8cbf12
sangshaofeng <3241113254@qq.com>
2018-04-09 15:54:41 +0800

    文章schema

--- mongodb/schema.js
+++ mongodb/schema.js
@@ -1,2 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const url = require('../config/config.js').url;
+
+mongoose.connect(url);
+
+// 文章schema
+exports.ArticleSchema = new Schema({
+  title: { type: String },
+  content: { type: String },
+  is_deleted: {
+    type: String,
+    default: '0'
+  },
+  meta: {
+    created_at: {
+      type: Date,
+      default: Date.now()
+    },
+    update_at: {
+      type: Date,
+      default: Date.now()
+    }
+  }
+})
