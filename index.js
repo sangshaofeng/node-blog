c494da105e9fa58c8cb66dd5b501c671c92cabf6
sangshaofeng <3241113254@qq.com>
2018-04-09 15:17:20 +0800

    启动监听端口

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = require('./config/config.js').port;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(less(__dirname + '/public'));              // 配置less文件目录，必须放在配置静态资源前面，否则不会重新编译
+app.use(express.static(__dirname + '/public'));    // 配置静态文件目录
+
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    console.log('App listening at http://%s:%s', host, port);
+});
