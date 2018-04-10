628c2d9f90600815a9915b241b3de263bb982a76
sangshaofeng <3241113254@qq.com>
2018-04-10 11:54:08 +0800

    更新启动文件

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const routes = require('./routes/index');
 const express = require('express');
 const app = express();
 const path = require('path');
 const port = require('./config/config.js').port;
+const less = require('less-middleware');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,7 +12,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(less(__dirname + '/public'));              // 配置less文件目录，必须放在配置静态资源前面，否则不会重新编译
 app.use(express.static(__dirname + '/public'));    // 配置静态文件目录
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// 注册路由
+routes(app);
+
+console.log(port)
+
 var server = app.listen(port, function () {
     var host = server.address().address;
-    console.log('App listening at http://%s:%s', host, port);
+    var _port = server.address().port;
+    console.log('App listening at http://%s:%s', host, _port);
 });
