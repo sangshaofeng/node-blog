f88e9a1b2ba0140f63d5b15be7bcf61404180348
sangshaofeng <3241113254@qq.com>
2018-04-12 18:08:18 +0800

    根据分类标签查询文章

--- routes/category.js
+++ routes/category.js
@@ -5,8 +5,19 @@ const ArticleCate = require('../mongodb/schema').ArticleCate;
 
 var Category = mongoose.model('category',  ArticleCate);
 
+// 获取全部标签
+router.get('/tags', function (req, res, next) {
+  Category.find({}, function (err, doc) {
+    if (!err) { 
+      res.json({ data: doc, msg: '获取成功', status: 'succ' })
+    } else {
+      res.json({ msg: '获取失败', status: 'err' })
+    }
+  })
+})
+
 // 添加标签
-router.post('/', function (req, res, next) {
+router.post('/tags', function (req, res, next) {
   const labelObject = {
     label: req.body.label
   }
