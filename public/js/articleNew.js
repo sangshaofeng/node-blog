482637e02a4853d87a3ba3aa60907e5d5b08b0e5
sangshaofeng <3241113254@qq.com>
2018-04-17 11:06:47 +0800

    添加文章js文件

---
+++ public/js/articleNew.js
@@ -0,0 +1,72 @@
+// 新建文章
+
+(function () {
+
+  var params = {
+    title: '',
+    content: '',
+    summary: '',
+    author: '',
+    category: '',
+    cateLabel: ''
+  }
+
+  getCateTags();
+
+  $('#submit-btn').click(function () {
+    params.title = $('input[name=title]').val();
+    params.content = $('textarea[name=content]').val();
+    params.summary = $('input[name=summary]').val();
+    params.author = $('input[name=author]').val();
+    params.category = $('select[name=category]').val();
+    params.cateLabel = $('select[name=category]').find('option:selected').text();
+    console.log(params)
+  })
+
+  // 获取全部分类标签
+  function getCateTags () {
+    $.ajax({
+      url: '/category/tags',
+      type: 'get',
+      dataType: 'json',
+      success: function (res) {
+        if (res.status === 'succ') {
+          appendTags(res.data);
+        }
+      }
+    })
+  }
+
+  // 填充标签
+  function appendTags (tags) {
+    var select = $('select[name=category]')
+    select.empty();
+    for (var i = 0; i < tags.length; i++) {
+      var option = '<option value="'+ tags[i]._id +'">'+ tags[i].label +'</option>';
+      select.append(option);
+    }
+  }
+
+  // 提交文章数据
+  function submitArticle (params) {
+    $.ajax({
+      url: '/article',
+      type: 'post',
+      data: '',
+      dataType: 'json',
+      success: function (res) {
+
+      }
+    })
+  }
+
+  // 重置输入框值
+  function resetValue () {
+    $('input[name=title]').val('');
+    $('textarea[name=content]').val('');
+    $('input[name=summary]').val('');
+    $('input[name=author]').val('');
+    $('select[name=category]').val('');
+  }
+
+})()
