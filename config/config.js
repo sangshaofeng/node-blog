e0e3fc0cdc957d9c9c28944f6ca5d4395d7e9f86
sangshaofeng <shaofeng1022>
2018-05-01 13:41:25 +0800

    修改端口号

--- config/config.js
+++ config/config.js
@@ -1,14 +1,6 @@
 var port = 3000;
 var url = 'mongodb://localhost/blog';   // mongo url
 
-var ENV = process.env.NODE_ENV;
-
-// 生产环境
-if (ENV === 'production') {
-  port = 8000;
-  url = 'mongodb://localhost/blog';
-}
-
 module.exports = {
   port: port,
   url: url
