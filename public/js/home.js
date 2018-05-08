5bc568ac1e2cdd1d78244b61bf5ba3d251fbedd2
sangshaofeng <3241113254@qq.com>
2018-05-08 18:32:40 +0800

    调整字段显示

--- public/js/home.js
+++ public/js/home.js
@@ -64,13 +64,12 @@
       var item = `<div class="article-item">
         <div class="article-header">
           <span>作者：<span>${doc[i].author}</span></span>
-          <span class="published-at">发布时间：${doc[i].meta.createdAt}</span>
+          <span class="category">分类：${doc[i].cateLabel}</span>
         </div>
         <h3><a href="/article/details?id=${doc[i]._id}">${doc[i].title}</a></h3>
         <p>${doc[i].summary}</p>
         <div class="article-footer">
           <div class="comments-wrapper">
-            <span class="category">分类：${doc[i].cateLabel}</span>
             <svg class="Zi Zi--Comment Button-zi" fill="currentColor" viewBox="0 0 24 24" width="1.2em" height="1.2em">
               <path d="M10.241 19.313a.97.97 0 0 0-.77.2 7.908 7.908 0 0 1-3.772 1.482.409.409 0 0 1-.38-.637 5.825 5.825 0 0 0 1.11-2.237.605.605 0 0 0-.227-.59A7.935 7.935 0 0 1 3 11.25C3 6.7 7.03 3 12 3s9 3.7 9 8.25-4.373 9.108-10.759 8.063z" fill-rule="evenodd"></path>
             </svg>
